837767f7a13d9153ba729d070622e5c77441dc9c
ItsLucas93 <45604254+ItsLucas93@users.noreply.github.com>
2021-03-22 14:46:33 +0100

    Update main.py

--- Tp_Images_Victor/main.py
+++ Tp_Images_Victor/main.py
@@ -3,9 +3,8 @@
 from math import sqrt
 import PIL.Image as img
 import numpy as np
-
-
-# import os
+import os
+import tqdm
 
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
@@ -18,6 +17,16 @@ def create_matrice(L, l):
     """
     return np.zeros((L, l))
 
+def reconstitution(matrice):
+    """ Recréer et renvoie une image à partir d'une matrice. """
+    longueur = matrice.shape[0]
+    hauteur = matrice.shape[1]
+    image = img.new("RGB", (longueur, hauteur))
+    for i in range(longueur):
+        for j in range(hauteur):
+            clr = int(matrice[i, j] * 255)
+            image.putpixel((i, j), (clr, clr, clr))
+    return image
 
 def lecture_image(img):
     (longueur, hauteur) = img.size
@@ -98,6 +107,42 @@ def parcours_l(debutA, finA, step, debutB, finB, matrice):
     return borne
 
 
+def ligne_horizontale(matrice, ep):
+    (L, l) = matrice.shape
+    ligne = matrice[L // 2]
+    nb_paquets = 0
+    for i in range(l - ep):
+        if ligne[i] == 0.0 or i == 0:
+            checkeur = True
+            for nb in range(1, ep + 1):
+                if not (ligne[i + nb] == 1.0):
+                    checkeur = False
+        else:
+            checkeur = False
+        if checkeur:
+            nb_paquets += 1
+    return nb_paquets
+
+
+def ligne_verticale(matrice, ep):
+    """ Même chose qu'avec 'ligne_horizontale' mais avec une ligne verticale du milieu. """
+    (L, l) = matrice.shape
+    nb_paquets = 0
+    colonne = l // 2
+    for i in range(L - ep):
+        if matrice[i][colonne] == 0.0 or i == 0:
+            checkeur = True
+            for nb in range(1, ep + 1):
+                if not (matrice[i + nb][colonne] == 1.0):
+                    checkeur = False
+        else:
+            checkeur = False
+        if checkeur:
+            nb_paquets += 1
+    return nb_paquets
+
+
+
 def matrice_rognee(matrice, marge):
     """ 
     Matrice rognée
@@ -109,3 +154,69 @@ def matrice_rognee(matrice, marge):
     borne_x1 = parcours_l(marge, l - marge, 1, marge, L - marge, matrice)
     borne_x2 = parcours_l(l - marge, marge, -1, marge, L - marge, matrice)
     return matrice[borne_y1:borne_y2 + 1, borne_x1:borne_x2 + 1]
+
+
+def matrice_final(matrice1):
+    """  Définit un matrice qui aura pour valeur soit 0 (noir) soit 1 (blanc) pour chaque membre.
+    Elle est déduite à partir de la matrice de Sobel. """
+    (L, l) = matrice1.shape
+    matrice2 = create_matrice(L, l)
+    for i in range(L):
+        for j in range(l):
+            if matrice1[i, j] < 0.16:
+                matrice2[i, j] = 1.0
+            # Comme 'matrice2' est au départ constituée uniquement de 0.0, on laisse cette valeur
+            # à 0.0 si elle est supérieure à 0.16, sinon elle passe à 1.0
+    return matrice2
+
+def Mise_en_place(path, L, l, **dico):
+    """ Définit les différents dictionnaires, listes, dimensions nécessaires à l'algorithme. """
+    liste0 = []
+    liste1 = []
+    liste2 = os.listdir(path)
+    size = (L, l)
+    for cle, valeur in dico.items():
+        for nb in range(valeur):
+            liste1.append(cle)
+    if len(liste1) != len(liste2):
+        raise ValueError("Il doit y avoir autant de types que d'images !")
+    return liste0, liste1, liste2, size
+
+
+def Mise_en_place_inconnu(path, L, l):
+    """ Définit la base de données, la liste des chemins menant aux images et les dimensions
+    nécessaires à l'algorithme. """
+    return [], os.listdir(path), (L, l)
+
+
+def Mise_en_image(matrice, num, rep, longueur, hauteur):
+    if num < 10:
+        name = rep + "/image0{}.png".format(num)
+    else:
+        name = rep + "/image{}.png".format(num)
+    image = reconstitution(matrice)
+    image = image.resize((longueur, hauteur))
+    image.save(name)
+    return image
+
+
+if __name__ == '__main__':
+    chemin = "train_data"
+
+    data_base, l_types, l_images, sizes = Mise_en_place(chemin, 400, 400, \
+                                                             Type_0=5, Type_1=8, Type_2=8, Type_3=5, Type_4=5, Type_5=9,
+                                                             Type_6=4, Type_7=4, Type_8=4, \
+                                                             Type_9=6)
+
+    longueur, hauteur = sizes
+
+    for i in range(tqdm(len(l_images))):
+        png = img.open(chemin + "/" + l_images[i])
+        png = png.resize((longueur, hauteur))
+        matrice = matrice_rognee(matrice_final(convolution(nuance_de_gris(png))), 40)
+        png = Mise_en_image(matrice, i, "train_data_finished", longueur, hauteur)
+        matrice = lecture_image(png)
+        paquet_larg = ligne_horizontale(matrice, 1)
+        paquet_long = ligne_verticale(matrice, 1)
+        data_base.append([matrice, paquet_long, paquet_larg, l_types[i]])
+        png.close()
